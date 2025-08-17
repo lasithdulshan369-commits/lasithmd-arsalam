e30cc99e5fb6094e666c65ccfdda51414a42ec58
lasithdulshan369-commits <lasithdulshan369@gmail.com>
2025-08-17 19:31:03 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -42,7 +42,7 @@ async (conn, mek, m, { from, quoted, sender, reply }) => {
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363348739987203@newsletter',
-                    newsletterName: "𝐀𝐫𝐬𝐥𝐚𝐧_𝐌𝐃",
+                    newsletterName: "LASITH-MD",
                     serverMessageId: 143
                 }
             }
